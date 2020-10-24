8d847a44dbb781d69a19ce3fcabad83ded11e4d4
Jason <28811675+xynoipse@users.noreply.github.com>
2020-10-24 13:24:16 +0800

    add bootcamps db seeder

---
+++ database/seeder.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const Bootcamp = require('../models/Bootcamp');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported...');
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed...');
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
